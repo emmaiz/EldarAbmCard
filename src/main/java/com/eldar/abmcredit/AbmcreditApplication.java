b9c5a9aeae9f4f9da9175f6583bb4db57e9a74ca
emmanuel maiz <emmanuelmaiz.globant@latam.com>
2024-11-28 00:50:02 -0300

    first commit

---
+++ src/main/java/com/eldar/abmcredit/AbmcreditApplication.java
@@ -0,0 +1,13 @@
+package com.eldar.abmcredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbmcreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbmcreditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eldar/abmcredit/AbmcreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eldar.abmcredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbmcreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
